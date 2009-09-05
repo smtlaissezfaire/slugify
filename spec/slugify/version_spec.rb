37bcef4392fcc2cd7c9f330f3601283a41b6eb06
Scott Taylor <scott@railsnewbie.com>
2009-09-05 14:07:06 -0400

    Version bum: 0.1.1

--- spec/slugify/version_spec.rb
+++ spec/slugify/version_spec.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 module Slugify
   describe VERSION do
-    it "should be at 0.1.0" do
-      Slugify::VERSION.should == "0.1.0"
+    it "should be at 0.1.1" do
+      Slugify::VERSION.should == "0.1.1"
     end
   end
 end
