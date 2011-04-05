aba4dcf4153730d4881435509076bb5e9d4d00c7
ralph <ralph@rvdh.de>
2011-04-05 11:23:55 +0200

    fix slug generator for ruby 1.9

--- lib/slugify/slug_generator.rb
+++ lib/slugify/slug_generator.rb
@@ -6,7 +6,7 @@ module Slugify
     
     class << self
       def generate_slug(str)
-        str = Iconv.iconv(CHAR_ENCODING_TRANSLATION_TO, CHAR_ENCODING_TRANSLATION_FROM, str).to_s
+        str = Iconv.iconv(CHAR_ENCODING_TRANSLATION_TO, CHAR_ENCODING_TRANSLATION_FROM, str).join
         str.downcase!
         
         str.gsub! /<.*?>/,                       ''   # strip HTML
